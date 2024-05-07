274153f8fc3276a74ee87ad8b1c53a036a3aeb86
ldhoon0813@gmail.com <ldhoon0813@gmail.com>
2024-05-08 01:35:38 +0900

    [DEP] Initial commit

---
+++ src/main/java/com/youngustandard/youngu_server/YoungUServerApplication.java
@@ -0,0 +1,13 @@
+package com.youngustandard.youngu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungUServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungUServerApplication.class, args);
+    }
+
+}
