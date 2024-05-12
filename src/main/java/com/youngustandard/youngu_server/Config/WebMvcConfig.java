c804dd3fe0fb3c6f6b3dfea7b71f2f3864c60ebb
ldhoon0813@gmail.com <ldhoon0813@gmail.com>
2024-05-12 16:54:58 +0900

    [FEAT] 백엔드 CORS 설정 Config 추가

---
+++ src/main/java/com/youngustandard/youngu_server/Config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.youngustandard.youngu_server.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // 스프링 빈으로 등록
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS=3000;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        //모든 경로에 대해
+        corsRegistry.addMapping("/**")
+        //Origin이 http:localhost:3000에 대해
+        .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
